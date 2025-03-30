b6a75a3c410203dcdf195b5b12a3de6b73cabd99
Wumpie7 <chrisjaver@gmail.com>
2025-03-30 17:21:34 +0200

    Added folder check

--- scripts/generateMessage.js
+++ scripts/generateMessage.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 
 const messages = JSON.parse(process.env.DAILY_MESSAGES);
 const today = new Date();
-const index = today.getDay(); // 0 = Sunday, 6 = Saturday
+const index = today.getDay();
 const dailyMessage = messages[index];
 
 fs.mkdirSync('public', { recursive: true });
+
 fs.writeFileSync('public/dailyMessage.json', JSON.stringify({ message: dailyMessage }, null, 2));
